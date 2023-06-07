025cc86dc7ee9985c8c7f15d1f09f23e33c74237
sinjaesung <gotodongyang@naver.com>
2023-06-08 00:53:42 +0900

    etude cosmetic added wow

---
+++ etude/swiperRelated.js
@@ -0,0 +1,54 @@
+//메인페이지
+var slide1=new Swiper('.slide1',{
+    slidesPerView:'auto',
+    autoResize: true,
+    /*pagination:{
+        el :'.swiper-pagination2',
+        type :'progressbar'
+    },
+    
+    navigation:{
+        nextEl : '.swiper-button-next2',
+        prevEl : '.swiper-button-prev2'
+    },*/
+})
+var slide2=new Swiper('.slide2',{
+    slidesPerView:'auto',
+    autoResize: true,
+   /* pagination:{
+        el :'.swiper-pagination2',
+        type :'progressbar'
+    }
+    navigation:{
+        nextEl : '.swiper-button-next2',
+        prevEl : '.swiper-button-prev2'
+    },*/
+})
+
+
+//제품상세페이지
+var prodetailLeftPCslide=new Swiper('.prodetailLeftPCslide',{
+    slidesPerView:'auto',
+    autoResize: true,
+    pagination:{
+        el :'.swiper-pagination2',
+        type :'progressbar'
+    },
+    /*
+    navigation:{
+        nextEl : '.swiper-button-next2',
+        prevEl : '.swiper-button-prev2'
+    },*/
+})
+var prodetailRightTMslide=new Swiper('.prodetailRightTMslide',{
+    slidesPerView:'auto',
+    autoResize: true,
+    pagination:{
+        el :'.swiper-pagination2',
+        type :'progressbar'
+    }/*
+    navigation:{
+        nextEl : '.swiper-button-next2',
+        prevEl : '.swiper-button-prev2'
+    },*/
+})
