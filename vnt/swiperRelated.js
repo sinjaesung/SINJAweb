ec263673fbd0ac052ed52b0c590849e17382f9b9
sinjaesung <gotodongyang@naver.com>
2023-06-08 15:32:29 +0900

    vnt site added wow

---
+++ vnt/swiperRelated.js
@@ -0,0 +1,24 @@
+var slide1=new Swiper('.slide1',{
+    slidesPerView:'auto',
+    autoResize: true,
+    /*pagination:{
+        el :'.swiper-pagination_style6',
+        type :'progressbar'
+    },
+    navigation:{
+        nextEl : '.swiper-button-next2',
+        prevEl : '.swiper-button-prev2'
+    },*/
+})
+var slide2=new Swiper('.slide2',{
+    slidesPerView:'auto',
+    autoResize: true,
+    /*pagination:{
+        el :'.swiper-pagination_style6',
+        type :'progressbar'
+    },
+    navigation:{
+        nextEl : '.swiper-button-next2',
+        prevEl : '.swiper-button-prev2'
+    },*/
+})
